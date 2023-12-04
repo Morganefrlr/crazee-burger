89f465e02946f3d40054ac393a3fc4761fe1580d
Morganefrlr <morganeancelin@me.com>
2023-12-04 13:00:03 +0100

    ✨ F09: EDDITP > add card hover

--- src/components/pages/order/Main/Menu/Card.jsx
+++ src/components/pages/order/Main/Menu/Card.jsx
@@ -1,25 +1,31 @@
 import styled from "styled-components";
 import Price from "./Price";
 import { theme } from "../../../../../theme";
-import {TiDelete} from 'react-icons/ti'
+import { TiDelete } from "react-icons/ti";
 import { useContext } from "react";
 import AdminContext from "../../../../../context/AdminContext";
 import ButtonComponent from "../../../../reusable/ButtonComponent";
 
-
-const Card = ({ img, title, price, onClick,id }) => {
-  const {adminMode} = useContext(AdminContext)
-
+const Card = ({ img, title, price, onClick, id }) => {
+  const { adminMode } = useContext(AdminContext);
 
   return (
     <CardStyled>
-      {img ? (<img src={img} alt={title} />) : (<img src='/images/coming-soon.png' alt={title} />)}
-      {adminMode && <div className="deleteIconContainer" onClick={() => onClick(id)}><TiDelete className="icon"/></div>}
+      {img ? (
+        <img src={img} alt={title} />
+      ) : (
+        <img src="/images/coming-soon.png" alt={title} />
+      )}
+      {adminMode && (
+        <div className="deleteIconContainer" onClick={() => onClick(id)}>
+          <TiDelete className="icon" />
+        </div>
+      )}
       <div className="cardBottom">
         <h3>{title}</h3>
         <div>
           <Price price={price} />
-          <ButtonComponent label={"Ajouter"} version='small' />
+          <ButtonComponent label={"Ajouter"} version="small" />
         </div>
       </div>
     </CardStyled>
@@ -38,26 +44,28 @@ const CardStyled = styled.div`
   gap: 15px;
   padding: 50px 20px 30px 20px;
   position: relative;
+  cursor: pointer;
+  &:hover {
+    box-shadow: 0 0 8px 0 ${theme.colors.primary};
+  }
   img {
     width: 200px;
     height: 145px;
     object-fit: contain;
   }
-  .deleteIconContainer{
-
+  .deleteIconContainer {
     position: absolute;
     top: 15px;
     right: 15px;
-    .icon{
+    .icon {
       width: 30px;
       height: 30px;
       fill: ${theme.colors.primary};
       cursor: pointer;
-      &:hover{
+      &:hover {
         fill: ${theme.colors.red};
       }
     }
-
   }
   .cardBottom {
     display: flex;
@@ -77,7 +85,6 @@ const CardStyled = styled.div`
       display: flex;
       justify-content: space-between;
       align-items: center;
-      
     }
   }
 `;
